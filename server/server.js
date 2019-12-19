7b3d332d222ce987b4b3c33bdb6894d10c4a4a42
Tammie Chung <tammie.chung11@gmail.com>
2019-12-19 09:50:25 -0500

    Update bodyparser

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use((bodyParser.urlencoded({extended: true})));
 
-app.listen(process.env.PORT, () => {
-  console.log(`app running on port ${process.env.PORT}`)
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`)
 });
