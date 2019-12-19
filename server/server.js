75361148c4ad1459016a20e732b660f8f5ee021b
Tammie Chung <tammie.chung11@gmail.com>
2019-12-19 11:09:40 -0500

    Import passport and start experimenting with oauth

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config({ path: './.env' });
 
+// SERVER CONFIG
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8000;
+const passport = require('passport');
+const GoodreadsStrategy = require('passport-goodreads').Strategy;
 
 const app = express();
 
@@ -11,3 +14,25 @@ app.use((bodyParser.urlencoded({extended: true})));
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
 });
+
+// OAUTH
+
+// Passport session setup
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Use Goodreads Strategy
+passport.use(new GoodreadsStrategy({
+  consumerKey: process.env['API_KEY'],
+    consumerSecret: process.env['API_SECRETKEY'],
+    callbackURL: "http://localhost:8000/auth/goodreads/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    // WHAT GOES HERE????
+  }
+));
